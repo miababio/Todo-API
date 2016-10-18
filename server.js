bc3e52b1a372532a6905ccac47ba1b22713ead9d
Michael Ababio <moa003@amfam.com>
2016-10-18 17:41:12 -0500

    Modified server.js; Added Notes

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: "Meet dad for lunch",
+    completed: false
+}, {
+    id: 2,
+    description: "Go to the store",
+    completed: false
+}, {
+    id: 3,
+    description: "Finish this video",
+    completed: true
+}];
 
 app.get("/", function (req, res) {
     res.send("Todo API Root");
 });
+
+// GET /todos
+app.get("/todos", function(req, res) {
+    res.json(todos);
+});
+
+// GET /todos:id
 
 app.listen(PORT, function() {
    console.log(`Express listening on port ${PORT}!`);
