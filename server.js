54006583215a8c0a3515a5c4a8ded736faafcc1c
Michael Ababio <moa003@amfam.com>
2016-11-01 17:57:43 -0500

    Finished more sections; Added more notes

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: "Meet dad for lunch",
-    completed: false
-}, {
-    id: 2,
-    description: "Go to the store",
-    completed: false
-}, {
-    id: 3,
-    description: "Finish this video",
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
     res.send("Todo API Root");
@@ -37,6 +30,19 @@ app.get("/todos/:id", function(req, res) {
     }
     matched !== null? res.json(matched) : res.status(404).send();
 });
+
+// POST /todos
+app.post("/todos", function(req, res) {
+    var body = req.body;
+    
+    //add id field
+    body.id = todoNextId++;
+    //push body in array
+    todos.push(body);
+    
+    res.json(body);
+});
+
 
 app.listen(PORT, function() {
    console.log(`Express listening on port ${PORT}!`);
