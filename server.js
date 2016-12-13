987f900c00b2c670ceed3f8efbb466a2459f37b7
Michael Ababio <moa003@amfam.com>
2016-12-13 17:06:03 -0600

    Convert GET /todos to Sequelize; Added more notes

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ app.get("/", function (req, res) {
 
 // GET /todos?completed=true&q=house
 app.get("/todos", function(req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
-
-    if(queryParams.hasOwnProperty("completed") && queryParams.completed === 'true')
-        filteredTodos = _.where(filteredTodos, {completed: true});
-    else if(queryParams.hasOwnProperty("completed") && queryParams.completed === 'false')
-        filteredTodos = _.where(filteredTodos, {completed: false}); 
+    var query = req.query;
+    var where = {};
     
-    if(queryParams.hasOwnProperty("q") && queryParams.q.length > 0)
-        filteredTodos = _.filter(filteredTodos, function(todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) !== -1? true : false; 
-        });
-    
-    res.json(filteredTodos);
+    if(query.hasOwnProperty("completed") && query.completed === 'true')
+        where.completed = true;
+    else if(query.hasOwnProperty("completed") && query.completed === 'false')
+        where.completed = false;
+    if(query.hasOwnProperty("q") && query.q.length > 0)
+        where.description = {$like: "%" + query.q + "%"};
+    db.todo.findAll({where: where}).then(function(todos) {
+        res.json(todos);
+    }).catch(function(e) {
+        res.status(500).send();
+    });
 });
 
 // GET /todos:id
